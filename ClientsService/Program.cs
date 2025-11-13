01c3a5de728dfdc916467dae63d7f05b541c940c
Yamir <yamcast12@gmail.com>
2025-11-13 13:41:19 -0300

    feat: add clients data seeder

--- ClientsService/Program.cs
+++ ClientsService/Program.cs
@@ -11,13 +11,12 @@ var builder = WebApplication.CreateBuilder(args);
 //builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
-
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddFluentValidationAutoValidation();
 builder.Services.AddFluentValidationClientsideAdapters();
-
 builder.Services.AddValidatorsFromAssemblyContaining<ClientCreateValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<ClientUpdateValidator>();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -33,6 +32,13 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+
+// Implementar el Data Seeding
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    DataSeeder.Initialize(services);
+}
 
 app.UseCors("AllowAll");
 app.UseAuthorization();
---
+++ ClientsService/Src/Data/DataSeeder.cs
@@ -0,0 +1,52 @@
+using Bogus;
+using ClientsService.Src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClientsService.Src.Data
+{
+    /// <summary>
+    /// Seeding inicial de datos en la base de datos.
+    /// </summary>
+    public class DataSeeder
+    {
+        /// <summary>
+        /// Método para inicializar el seeding de datos.
+        /// </summary>
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<AppDbContext>() ??
+                throw new InvalidOperationException("No se pudo obtener el contexto de la base de datos.");
+
+            context.Database.MigrateAsync();
+
+            if (context.Clients.Any())
+            {
+                return;
+            }
+
+            var faker = new Faker("es");
+
+            var clients = new List<Client>();
+            for (int i = 0; i < 20; i++)
+            {
+                var client = new Client
+                {
+                    Id = Guid.NewGuid(),
+                    FullName = faker.Name.FullName(),
+                    Email = faker.Internet.Email(),
+                    Username = faker.Internet.UserName(),
+                    BirthDate = DateOnly.FromDateTime(faker.Date.Past(30, DateTime.Now.AddYears(-18))),
+                    Address = faker.Address.FullAddress(),
+                    PhoneNumber = faker.Phone.PhoneNumber("+569########"),
+                    PasswordHash = BCrypt.Net.BCrypt.HashPassword(faker.Internet.Password(10, false, "", "Aa1!")),
+                    IsActive = true,
+                    CreatedAt = DateTime.UtcNow,
+                };
+                clients.Add(client);
+            }
+
+            context.Clients.AddRange(clients);
+            context.SaveChanges();
+        }
+    }
+}
